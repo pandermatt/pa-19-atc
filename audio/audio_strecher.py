b13cc9f079fd133e01f3cfc9bfce32045e4ea507
Pascal Andermatt <p.andermatt@me.com>
2019-10-12 22:03:52 +0200

    Add noise injection + librosa, numpy

--- audio/audio_strecher.py
+++ audio/audio_strecher.py
@@ -1,6 +1,8 @@
 import glob
 import os
 
+import librosa
+import numpy as np
 from pydub import AudioSegment
 from pydub.effects import speedup
 from pydub.utils import which
@@ -20,13 +22,15 @@ def convert_audio_files():
         for speed in [1.1]:
             _convert_audio_with_normalisation(original_audio_file_path, speed)
 
+        _convert_audio_with_noise_injection(original_audio_file_path, 0.01)
+
 
 def _convert_audio(audio_file_path, speed):
     file_name = os.path.basename(audio_file_path)
     output_path = os.path.join(config.clean_data_custom_audio_dir(prefix=str(speed)), file_name)
 
     if os.path.exists(output_path):
-        log.info(f'{file_name}-{speed} already exists... skipping')
+        log.info(f'already exists... skipping:\t {file_name}-{speed}')
         return
 
     log.info(f'Convert {file_name} with {speed}')
@@ -43,11 +47,29 @@ def _convert_audio_with_normalisation(audio_file_path, speed):
     output_path = os.path.join(config.clean_data_custom_audio_dir(prefix=str(speed) + '-normal'), file_name)
 
     if os.path.exists(output_path):
-        log.info(f'{file_name}-{speed}-normalisation already exists... skipping')
+        log.info(f'already exists... skipping:\t {file_name}-{speed}-normalisation')
         return
 
     log.info(f'Convert {file_name} with 1.1 normalisation')
     os.system(f'ffmpeg -i {audio_file_path}  -filter:a "atempo={speed}" {output_path}')
+
+
+def _convert_audio_with_noise_injection(audio_file_path, noise_factor):
+    file_name = os.path.basename(audio_file_path)
+    output_path = os.path.join(config.clean_data_custom_audio_dir(prefix=str(noise_factor) + '-noise'), file_name)
+
+    if os.path.exists(output_path):
+        log.info(f'already exists... skipping:\t {file_name}-{noise_factor}-noise')
+        return
+
+    log.info(f'Convert {file_name} with noise injection')
+    data, sr = librosa.load(audio_file_path)
+
+    noise = np.random.randn(len(data))
+    augmented_data = data + noise_factor * noise
+    augmented_data = augmented_data.astype(type(data[0]))
+
+    librosa.output.write_wav(output_path, augmented_data, sr)
 
 
 if __name__ == '__main__':

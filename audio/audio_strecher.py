66b36ca20cf677397ba9cbb23a5f5c1a6d65fb0f
Pascal Andermatt <p.andermatt@me.com>
2019-10-14 18:42:07 +0200

    Fix failing audios

--- audio/audio_strecher.py
+++ audio/audio_strecher.py
@@ -15,14 +15,23 @@ def convert_audio_files():
     if not which("ffmpeg"):
         log.warning("Couldn't find ffmpeg")
 
+    error_files = []
+
     for original_audio_file_path in glob.glob(os.path.join(config.clean_data_audio_dir(), '*.wav')):
-        for speed in [0.9, 1.1, 1.3]:
-            _convert_audio(original_audio_file_path, speed)
+        try:
+            for speed in [0.9, 1.1, 1.3]:
+                _convert_audio(original_audio_file_path, speed)
 
-        for speed in [1.1]:
-            _convert_audio_with_normalisation(original_audio_file_path, speed)
+            for speed in [1.1]:
+                _convert_audio_with_normalisation(original_audio_file_path, speed)
 
-        _convert_audio_with_noise_injection(original_audio_file_path, 0.01)
+            _convert_audio_with_noise_injection(original_audio_file_path, 0.01)
+        except:
+            error_files.append(original_audio_file_path)
+            log.warning(f'Could not convert {original_audio_file_path}')
+
+    if len(error_files) != 0:
+        log.warning(f'Could not convert {error_files}')
 
 
 def _convert_audio(audio_file_path, speed):

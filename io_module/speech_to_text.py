f6067e1f732f7c8320017cd2c401135663a1f6b4
Jennifer Schürch <jennifermschuerch@gmail.com>
2019-10-26 23:28:05 +0200

    Make speech-to-text a little more flexible

--- io_module/speech_to_text.py
+++ io_module/speech_to_text.py
@@ -10,8 +10,12 @@ from speech.speech_to_text import speech_to_text
 
 
 def speech_to_text_save_to_file():
-    for audio_file_path in glob.glob(join(config.test_data_audio_dir(), '*.wav')):
-        text_file_path = join(config.provider_accuracy_dir(),
+    #for audio_file_path in glob.glob(join(config.test_data_audio_dir(), '*.wav')):
+    #    text_file_path = join(config.provider_accuracy_dir(),
+    #                          basename(audio_file_path).replace('.wav', '.txt'))
+    type = "_noise-0.03"
+    for audio_file_path in glob.glob(join(config.test_data_audio_dir(suffix=type, subdir="converted"), '*.wav')):
+        text_file_path = join(config.provider_accuracy_dir(prefix="test"+type+"_"),
                               basename(audio_file_path).replace('.wav', '.txt'))
 
         if exists(text_file_path):

abfd8499ad67c25887e327cca46917de06e2cae3
Pascal Andermatt <p.andermatt@me.com>
2019-11-06 21:18:07 +0100

    Small changes

--- audio/plot_audio.py
+++ audio/plot_audio.py
@@ -9,8 +9,8 @@ from config import config
 
 if __name__ == '__main__':
     # samplingFrequency, signalData = wavfile.read(os.path.join(config.test_data_dir(), "audio_speed-1.3/gf1_01_002_speed-1.3.wav"))
-    samplingFrequency, signalData = wavfile.read(os.path.join(config.test_data_dir(), "audio_speed-1.1-normal/gf1_01_002_speed-1.1-normal.wav"))
-    # samplingFrequency, signalData = wavfile.read(os.path.join(config.test_data_dir(), "audio_noise-0.09/gf1_01_002_noise-0.09.wav"))
+    # samplingFrequency, signalData = wavfile.read(os.path.join(config.test_data_dir(), "audio_speed-1.1-normal/gf1_01_002_speed-1.1-normal.wav"))
+    samplingFrequency, signalData = wavfile.read(os.path.join(config.test_data_dir(), "audio_noise-0.03/gf1_01_002_noise-0.03.wav"))
     plot.subplot(211)
     plot.title('Spektogramm einer WAV Datei')
     plot.plot(signalData / np.max(np.abs(signalData)))
